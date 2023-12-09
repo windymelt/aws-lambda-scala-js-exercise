0ed12526b1f247e9a44f491bdd986eff34d58b42
windymelt <windymelt@3qe.us>
2023-12-09 16:12:18 +0900

    build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+sbt fullOptJS
+
+if [[ -f lambda.zip ]]; then
+  rm lambda.zip
+fi
+
+if [[ -d lambda ]]; then
+  rm -rf lambda
+fi
+
+mkdir lambda
+cp target/scala-3.3.1/aws-lambda-scala-js-exercise-opt/* lambda/
+cp -r node_modules lambda/
+cd lambda
+zip -r ../lambda.zip *
